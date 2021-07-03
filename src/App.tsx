497f7cca17ffd5d8bee49a989d65606f81383e83
John M <jmmabanta51@gmail.com>
2021-07-03 14:37:12 -0600

    Added input for data set size

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Component } from "react";
+import React from "react";
 import "./App.css";
+import { useDataSize } from "./components/SetDataSize";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sorting Algorithms</h1>
-        <h3>
-          <i>*still a work in progress don't flame :(*</i>
-        </h3>
+const App = () => {
+  const [value, add, subtract, handleChange] = useDataSize(1);
+
+  return (
+    <div className="App">
+      <h1>Sorting Algorithms</h1>
+      <h3>
+        <i>*still a work in progress don't flame :(*</i>
+      </h3>
+      <div className="data-size-form">
+        <input name="dataSize" value={value} onChange={handleChange}></input>
+        <div className="data-size-form-buttons">
+          <button onClick={subtract}>-</button>
+          <button onClick={add}>+</button>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
