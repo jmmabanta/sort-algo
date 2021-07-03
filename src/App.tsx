20be1fb24fc34c099a896e71fb1b60d05dd9eb5b
John M <jmmabanta51@gmail.com>
2021-07-03 02:56:16 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Sorting Algorithms</h1>
+        <h3>
+          <i>*still a work in progress don't flame :(*</i>
+        </h3>
+      </div>
+    );
+  }
+}
