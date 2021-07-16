659f1f6ba62c947bb56ae12a475fc72f51fb4737
John M <johnmabanta51@gmail.com>
2021-07-16 01:26:19 -0600

    Renamed filed for consistency

---
+++ src/components/sort/SortDataInputs.tsx
@@ -0,0 +1,52 @@
+import { AlgorithmType } from './SortAnimator';
+
+interface ButtonProps {
+  setSpeed: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  sortData: (algorithm?: AlgorithmType) => void;
+  isSorted: boolean;
+  isDisabled: boolean;
+}
+
+const SortDataButtons = (props: ButtonProps) => {
+  return (
+    <div
+      className="input_section"
+      style={{
+        display: props.isSorted ? 'none' : 'inline'
+      }}
+    >
+      <h3>Sort Speed:</h3>
+      <input
+        type="range"
+        className="slider sort"
+        min={0.1}
+        max={1.0}
+        step={0.1}
+        defaultValue={0.5}
+        onInput={props.setSpeed}
+        onChange={props.setSpeed}
+        disabled={props.isDisabled}
+      ></input>{' '}
+      <div className="button_list sort_button">
+        <button
+          onClick={() => {
+            props.sortData('selection');
+          }}
+          disabled={props.isSorted}
+        >
+          Selection Sort
+        </button>
+        <button
+          onClick={() => {
+            props.sortData('insertion');
+          }}
+          disabled={props.isSorted}
+        >
+          Insertion Sort
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SortDataButtons;
